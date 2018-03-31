50e207e2426bf2f4837ab0645b655ede6b8f6301
Tamas Balogh <Tamas.Balogh@logmein.com>
2018-03-31 21:10:44 +0200

    Fix test

--- test_article_parser.py
+++ test_article_parser.py
@@ -1,17 +1,8 @@
 import unittest
-from article_parser import is_portal_accepted, get_output_path, from_nynyny_date_format, from_origo_date_format, validate_config
+from article_parser import get_output_path, from_nynyny_date_format, from_origo_date_format, validate_config
 
 
 class TestArticleParser(unittest.TestCase):
-
-    def test_accepted_portals(self):
-        test_map = {
-            'index': True, 'origo': True, 'nnn': True, 'nynyny': True, 'ps': True,
-            'hvg': False, '': False
-        }
-
-        for portal, expected in test_map.items():
-            self.assertEquals(is_portal_accepted(portal), expected)
 
     def test_get_output_path(self):
         test_map = { 
@@ -33,21 +24,11 @@ class TestArticleParser(unittest.TestCase):
             "2012. március 1. 09:12": "2012-03-01T09:12:00+00:00"
         }
         for date, expected in test_map.items():
-            self.assertEquals(from_nynyny_date_format([date]), expected)
+            self.assertEqual(from_nynyny_date_format([date]), expected)
 
     def test_valid_configs(self):
         valid_configs = [
-            {portal: 'index'}, {portal: 'origo'}, {portal: 'nnn'}, {portal: 'nynyny'}, {portal: 'ps'}
+            {'portal': 'index'}, {'portal': 'origo'}, {'portal': 'nnn'}, {'portal': 'nynyny'}, {'portal': 'ps'}
         ]
         for config in valid_configs:
             validate_config(config)
-
-    # TODO remove unused test
-    # def test_from_origo_date_format(self):
-    #     #YYYY. month DD. HH:MM
-    #     test_map = {
-    #         "\r\n\t\t\tvar strModDate = '2011. 08. 02., 18:08';\r\n\t\t\tif (strModDate.substr(16,1) == ':') strModDate = strModDate.substr(0,15) + '0' + strModDate.substr(15,strModDate.length);\r\n\t\t\tvar strNowDate = '2017. 09. 11., 01:03';\r\n\t\t   ": "2011-08-02T18:08:00+00:00",
-    #         "strModDate = '2012. 03. 01. 09:12'": "2012-03-01T09:12:00+00:00"
-    #     }
-    #     for date, expected in test_map.items():
-    #         assert(from_origo_date_format([date]) == expected)
